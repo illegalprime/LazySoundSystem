921dc3b9fe1c2c5392a9431dd374a18bf3fd777e
Michael Eden <themichaeleden@gmail.com>
2015-02-02 01:08:35 -0500

    Added spotify search

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
-var router = express.Router();
+var request = require('request');
+var router  = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/call/spotify/search', function(req, res) {
+    searchCall = 'https://api.spotify.com/v1/search';
+    request.get({url: searchCall, qs: req.query}, function(err, response, body) {
+        if (!err && response.statusCode == 200) {
+            res.send(response.body);
+        }
+        else res.status(500).send({});
+    });
 });
 
 module.exports = router;
