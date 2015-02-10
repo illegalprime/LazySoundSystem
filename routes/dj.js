000142e73d7df638159137061ddc1202eae16be1
Andrew Dai <theman@andrewdai.co>
2015-02-10 15:00:56 -0500

    functional/useful landing page

--- routes/dj.js
+++ routes/dj.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/', function(req, res, next) {
+    res.redirect('/dj/' + req.body.name);
+});
+
 router.get('/:id', function(req, res, next) {
     res.render('queue', { id: req.params.id });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router  = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The LazySoundSystem' });
 });
 
 router.get('/call/spotify/search', function(req, res) {
