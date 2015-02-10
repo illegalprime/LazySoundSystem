ab4e93ebd50637d51d766b6538748df736ea24ec
Andrew Dai <theman@andrewdai.co>
2015-02-10 01:19:26 -0500

    forgot to add gruntfile to last commit

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+        // TODO define tasks
+    });
+
+    // TODO need this for everything to work
+    // grunt.registerTask('default', ['uglify']);
+}
