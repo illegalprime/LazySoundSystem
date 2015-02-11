58fb6362f3c09d81e50d31bc01427f36b9c68676
Andrew Dai <theman@andrewdai.co>
2015-02-10 20:32:03 -0500

    WIP trying to get grunt working with regen on change
     - currently having issues with starting the node server
       - is there an alternative to `./bin/www` ?
     - `watch` and `jshint` are working now

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,43 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            dev: {
+                files: ['routes/*.js', 'app.js', 'public/javascripts/*.js'],
+                tasks: ['jshint', 'express:dev']
+            }
+        },
         jshint: {
-            all: ['*.js', 'public/**/*.js', 'routes/*.js']
+            options: {
+                node: true
+            },
+            all: {
+                src: ['*.js', 'public/**/*.js', 'routes/*.js']
+            }
+        },
+        express: {
+            dev: {
+                options: {
+                    script: './bin/www'
+                }
+            }
         }
     });
 
-    // load the plugin that provides the 'uglify' task
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // set 'uglify' as the default task
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', '', function() {
+        var taskList = [
+            'jshint',
+            'express:dev',
+            'watch'
+        ];
+        grunt.task.run(taskList);
+    });
 };
+
+// with help from stackoverflow <3
+// http://stackoverflow.com/questions/25073931/gruntjs-with-grunt-nodemon-watch-and-jshint
